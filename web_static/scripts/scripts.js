cb203091d4e971a2c00ee07383d5a6c581e32be2
GirumZ <girum.zebe@gmail.com>
2024-07-15 16:39:12 +0300

    [Updata]: linked up the sign up and sign in forms to the API

--- web_static/scripts/scripts.js
+++ web_static/scripts/scripts.js
@@ -24,3 +24,96 @@ function nextSlide() {
 function prevSlide() {
     showSlide(currentSlide - 3);
 }
+
+function submitSignupForm(event) {
+    event.preventDefault();
+
+    const userType = document.getElementById('user-type').value
+
+    if (userType === 'volunteer') {
+        var fetchedUrl = 'http://localhost:5000/auth/volunteer-register'
+        var formData = {
+            first_name: document.getElementById('first_name').value,
+            last_name: document.getElementById('last_name').value,
+            email: document.getElementById('volunteer-email').value,
+            password: document.getElementById('volunteer-password').value,
+        };
+    
+    }
+
+    if (userType === 'organization') {
+        var fetchedUrl = 'http://localhost:5000/auth/organization-register'
+        var formData = {
+            org_name: document.getElementById('org-name').value,
+            email: document.getElementById('org-email').value,
+            password: document.getElementById('org-password').value,
+        };
+    }
+
+    fetch(fetchedUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('You have successfuly signed up. Please sign In to continue')
+        console.log('sucess:', data);
+
+        window.location.href = 'signin.html';
+    })
+    .catch(error => {
+        alert('Error while trying to sign up')
+        console.error('Error:', error);
+    });
+}
+
+/*function showNotification(message) {
+    var notificationContainer = document.getElementById('notificationContainer');
+
+    var notification = document.createElement('div');
+    notification.className = 'notification';
+    notification.textContent = message;
+
+    notificationContainer.appendChild(notification);
+
+    setTimeout(() => {
+        notification.classList.add('show');
+    }, 100);
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+        setTimeout(() => {
+            notificationContainer.removeChild(notification);
+        },500);
+    }, 3000);
+}
+*/
+
+function submitSigninForm(event) {
+    event.preventDefault();
+
+    var formData = {
+        email: document.getElementById('user-email').value,
+        password: document.getElementById('user-password').value,
+        };
+
+    fetch('http://localhost:5000/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(`You have successfuly signed in as: ${data.id}`)
+        console.log('sucess:', data);
+    })
+    .catch(error => {
+        alert('Error while trying to sign in')
+        console.error('Error:', error);
+    });
+}
