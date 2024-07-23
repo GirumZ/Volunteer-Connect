4b39288046ec3002ac8fda6f7aad4dd8396978c2
GirumZ <girum.zebe@gmail.com>
2024-07-24 02:27:20 +0300

    [Update]: Added more ForeignKey relationship between models

--- app/models/application.py
+++ app/models/application.py
@@ -8,4 +8,17 @@ class Application(BaseModel, db.Model):
 
     volunteer_id = db.Column(db.String(36), db.ForeignKey('volunteers.id'), nullable=False)
     opportunity_id = db.Column(db.String(36), db.ForeignKey('opportunities.id'), nullable=False)
+    org_id = db.Column(db.String(36), db.ForeignKey('opportunities.org_id'))
+    org_name = db.Column(db.String(128), db.ForeignKey('opportunities.org_name'))
+    opp_title = db.Column(db.String(150), db.ForeignKey('opportunities.title'))
+    opp_description = db.Column(db.String(255), db.ForeignKey('opportunities.description'))
+    opp_start_date = db.Column(db.Date, db.ForeignKey('opportunities.start_date'))
+    opp_end_date = db.Column(db.Date, db.ForeignKey('opportunities.end_date'))
+    opp_start_date = db.Column(db.Date, db.ForeignKey('opportunities.start_date'))
+    contact_email = db.Column(db.String(100), db.ForeignKey('opportunities.contact_email'))
+    contact_phone = db.Column(db.String(20), db.ForeignKey('opportunities.contact_phone'))
+    location = db.Column(db.String(255), db.ForeignKey('opportunities.location'))
+
+
+
     status = db.Column(db.String(50), default='Pending')
--- app/models/organization.py
+++ app/models/organization.py
@@ -7,11 +7,11 @@ class Organization(User, db.Model):
     __tablename__ = 'organizations'
 
     id = db.Column(db.String(36), db.ForeignKey('users.id'), primary_key=True)
-    org_name = db.Column(db.String(128), nullable=False)
+    org_name = db.Column(db.String(128), nullable=False, index=True)
     est_date = db.Column(db.Date, nullable=True)
     mission_statement = db.Column(db.Text, nullable=True)
-    contact_email = db.Column(db.String(100), nullable=True)
-    contact_phone = db.Column(db.String(20), nullable=True)
+    contact_email = db.Column(db.String(100), nullable=True, index=True)
+    contact_phone = db.Column(db.String(20), nullable=True, index=True)
     location = db.Column(db.String(100), nullable=True)
     website_url = db.Column(db.String(255), nullable=True)
     profile_picture_url = db.Column(db.String(255), nullable=True)
